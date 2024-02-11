40cb5d9f511beb864c07c98453065592aea46a6c
Enigmo13 <szymon.muc4@gmail.com>
2024-02-11 11:39:46 +0100

    added more accurate timer, removed bug when timer hit 00:00:0;

--- js.js
+++ js.js
@@ -16,7 +16,6 @@ information_div.style.left=(x-155)+'px';
 information_div.style.top=(y-75)+'px';
 restart_button.style.left=(x-85)+'px';
 restart_button.style.top=(y+75)+'px';
-//div centering
 window.addEventListener('resize',centering);
 let scores=[];
 let is_timer_started=false;
@@ -32,15 +31,21 @@ let tmp3_null=0;
 let falstart=0;
 let falstart_count=0;
 let tmp_additional_clicks=0;
+let start_date=0;
+let end_date=0;
 function race_start() {
     const time_p=document.getElementById('your_time_p')
     //Show final time and end the counting
     if (is_timer_started) {
+        //measure accurate time
+        end_date=Date.now()-start_date;
         clearInterval(lights_time);
         information_div.classList.add('show_time');
-        let final_time=tmp_null+minutes+':'+tmp2_null+seconds+':'+tmp3_null+milliseconds;
-        time_p.innerText=final_time;
-        score_board_f(all_miliseconds, final_time);
+        end_date=String(end_date).slice(-3.-1)
+        let tmp_final_time=tmp_null+minutes+':'+tmp2_null+seconds+':'+tmp3_null+end_date;
+        console.log(tmp_final_time)
+        time_p.innerText=tmp_final_time;
+        score_board_f(tmp_final_time);
         is_timer_started=false;
     }else{
         if (!is_lights_on) {
@@ -73,6 +78,7 @@ function race_start() {
                         if (!falstart) {
                             //checking if random time ends
                             if (tmp_count1>random_time) {
+                            start_date=Date.now();
                             clearInterval(random_time_interv);
                             //turning off the lights
                             for (let i = 0; i < bulbs.length; i++) {
@@ -98,7 +104,7 @@ function race_start() {
             //First falstart check
             if (tmp_additional_clicks<1) {
                 falstart_count++;
-                score_board_f('x','FALSTART');
+                score_board_f('FALSTART');
                 if (falstart_count>=3) {
                     loser();
                 }
@@ -133,21 +139,24 @@ function timer_update() {
         }else{tmp2_null=''}
         if (milliseconds<10) {
             tmp3_null='0';
+        }else if (milliseconds==0) {
+            tmp3_null='00'
         }else{tmp3_null=''}
-        timer1.innerText="TIME: "+tmp_null+minutes+':'+tmp2_null+seconds+':'+tmp3_null+milliseconds; 
+        timer1.innerText="TIME: "+tmp_null+minutes+':'+tmp2_null+seconds+':'+tmp3_null+milliseconds;
     }else{
         return;
     }
     
 }
-function score_board_f(tmp_milliseconds, final) {
+//score board controll
+function score_board_f(final) {
     scores.unshift(final)
-    scores[0].key=tmp_milliseconds
     document.querySelectorAll("li").forEach(li => {
         li.remove();
     });
     scores.sort();
-    if (scores.length==11) {
+    //setting max scores on scoreboard to 10
+    while (scores.length>=11) {
         scores.pop();
     }
     scores.forEach(element => {
@@ -156,6 +165,7 @@ function score_board_f(tmp_milliseconds, final) {
         score_board.appendChild(score_element);
     });
 }
+//centering information_div and restart_button
 function centering() {
     x = window.innerWidth / 2;
     y = window.innerHeight / 2;
@@ -166,6 +176,7 @@ function centering() {
     restart_button.style.left=(x-85)+'px';
     restart_button.style.top=(y+75)+'px';
 }
+//losing after 3 falstarts
 function loser() {
     document.body.removeEventListener('keydown', space_check)
     document.body.removeEventListener('mousedown',race_start);
@@ -174,6 +185,7 @@ function loser() {
     information_div.style.left=(x-205)+'px';
     restart_button.classList.add('show_time')
 }
+//restart button function
 function restart() {
     document.body.addEventListener('mousedown',race_start);
     document.body.addEventListener('keydown', space_check);
@@ -184,6 +196,7 @@ function restart() {
     falstart_count=0;
     factory_options();
 }
+//returning everything to factoryoptions
 function factory_options() {
     //hiding the information div
     information_div.classList.remove('show_time');
